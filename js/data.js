363c73a36bd941b7d51df4c598b0374c55835afc
radhinmuhammed98 <radhinmuhammed98+1@gmail.com>
2026-04-10 16:55:15 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,26 @@
-const LOGO_DATA_URL = "logo.jpeg";
-
-const DEFAULT_DATA = {
+{
+  "Canva": {
+    "_icon": "🌆",
+    "_color": "#00a1d6",
+    "_links": [
+      {
+        "name": "ID Card Photo (8 copies)",
+        "url": "https://www.canva.com/design/DAHEFv8xyUk/PJHbu0reSSrMq2C1TMalQw/edit",
+        "desc": "Print 8 copies of Passport size photo",
+        "keywords": [
+          "passport"
+        ]
+      },
+      {
+        "name": "ID Photo (4 Copies)",
+        "url": "https://www.canva.com/design/DAHGc9B_fr0/ChLgCpFPAB1_U3xLNNw7uQ/edit?ui=e30&referrer=https%3A%2F%2Fwww.canva.com%2Fdesign%2FDAHFC_CtJ20%2Fqj65x60JNnXb_gQF4WLEvQ%2Fedit",
+        "desc": "Print 4 copies of Passport size photo",
+        "keywords": [
+          "passport size photo"
+        ]
+      }
+    ]
+  },
   "Certificates": {
     "_icon": "🖼",
     "_color": "#aeff00",
@@ -9,7 +29,9 @@ const DEFAULT_DATA = {
         "name": "Birth Certificate",
         "url": "https://ksmart.lsgkerala.gov.in/ui/cr/public/quick-certificate-birth",
         "desc": "Get Birth Certificate quickly",
-        "keywords": ["birth certificate"]
+        "keywords": [
+          "birth certificate"
+        ]
       },
       {
         "name": "Death Certificate",
@@ -508,11 +530,8 @@ const DEFAULT_DATA = {
         "desc": "Every services related with vehicle RC",
         "keywords": [
           "Vehicle related"
-        ],
+        ]
       }
-    ],
-    "Vehicle Registration": [
-      
     ]
   },
   "Health Services": {
